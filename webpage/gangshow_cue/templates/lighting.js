ea909924b850298352e1ccec05d2515bf2dba9bb
Luke Hovington <lukeh@hovo.id.au>
2019-06-21 23:22:02 +1000

    lighting cue page looks ok, basic implementation is done

--- webpage/gangshow_cue/templates/lighting.js
+++ webpage/gangshow_cue/templates/lighting.js
@@ -1,19 +1,39 @@
 
-function refresh() {
+function refresh()
+{
 	if (cueData && cueData.hasOwnProperty(currentCue))
 	{
-		presentCue = cueData[currentCue];
-		document.getElementById('Cue').innerHTML = !!currentCue === true ? currentCue : "?" ;
-		for (prop in presentCue)
+		var cueLength = document.getElementsByClassName("leftspot").length;
+		var keys = Object.keys(cueData);
+		var startIndex = keys.indexOf(currentCue.toString());
+
+		// Calculate the start so the current cue is in the middle
+		if (startIndex > Math.ceil(cueLength/2))
 		{
-			if(document.getElementById(prop))
-			{
-				document.getElementById(prop).innerHTML = presentCue[prop];
-			}
+			startIndex -= Math.ceil(cueLength/2);
 		}
+		else
+		{
+			startIndex = 0;
+		}
+
+		var leftNotes = document.getElementsByClassName("leftspot")
+		var cueNumber = document.getElementsByClassName("midcue")
+		var rightNotes = document.getElementsByClassName("rightspot")
+
+		for (i = 0; i < cueLength; i++) { 
+			presentCue = cueData[keys[startIndex+i]];
+
+			leftNotes[i].innerHTML = presentCue['Left Spot'].replace(/(?:\r\n|\r|\n)/g, '<br>')
+			cueNumber[i].innerHTML = keys[startIndex+i]
+			rightNotes[i].innerHTML = presentCue['Right Spot'].replace(/(?:\r\n|\r|\n)/g, '<br>')
+		}
+
+		
+		document.getElementById('Cue').innerHTML = !!currentCue === true ? currentCue : "?" ;
 	}
 	else
 	{
-		document.getElementById('Cue').innerHTML = "?";
+		document.getElementById('Cue').innerHTML = currentCue;
 	}
 }
