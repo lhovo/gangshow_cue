8d527d1a8d0abb8fbdc1167b3b798a22643dedbb
Tracey Wright <traceywright@Traceys-MBP.gateway>
2019-06-22 14:15:41 +1000

    cast page

--- webpage/gangshow_cue/templates/cast.js
+++ webpage/gangshow_cue/templates/cast.js
@@ -3,17 +3,25 @@ function refresh() {
 	if (cueData && cueData.hasOwnProperty(currentCue))
 	{
 		presentCue = cueData[currentCue];
-		document.getElementById('Cue').innerHTML = !!currentCue === true ? currentCue : "?" ;
+
+		document.getElementById('Cue').innerHTML = !!currentCue === true ? currentCue : "" ;
+		document.getElementById('bracket').innerHTML = !!currentCue === true ? ")" : "" ;
+		
+		// only say scene not loaded if there's a problem with the data
+		document.getElementById('next-scene').innerHTML = !!currentCue === true ? "Next Scene:" : "Scene not Loaded" ;
 		for (prop in presentCue)
 		{
 			if(document.getElementById(prop))
 			{
 				document.getElementById(prop).innerHTML = presentCue[prop];
 			}
+			if (prop === "CastCall") {
+				document.getElementById("CastCall").classList += " transition-class";
+			}
 		}
 	}
 	else
 	{
-		document.getElementById('Cue').innerHTML = "?";
+		// document.getElementById('Cue').innerHTML = "?";
 	}
 }
